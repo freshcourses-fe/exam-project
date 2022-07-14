fb8d1abdb14d8c0a8ffa159445c9cf681e2c938c
Ekachcka <2331286yan@gmail.com>
2022-07-14 21:03:54 +0300

    new transactionHistory

--- server/src/models/transactionhistory.js
+++ server/src/models/transactionhistory.js
@@ -1,45 +1,44 @@
 'use strict';
+const { Model } = require('sequelize');
 const {
-  Model
-} = require('sequelize');
+  TRANSACTION_TYPES: { INCOME, EXPENSE },
+} = require('../constants');
 module.exports = (sequelize, DataTypes) => {
-  class TransactionHistory extends Model {
+  class Transaction extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate ({User, Bank}) {
       // define association here
+      Transaction.belongsTo(User, {foreignKey: 'userId'});
+      Transaction.belongsTo(Bank, {foreignKey: 'cardNumber'});
     }
   }
-  TransactionHistory.init({
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER
+  Transaction.init(
+    {
+      userId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      type: {
+        allowNull: false,
+        type: DataTypes.ENUM(INCOME, EXPENSE),
+      },
+      amount: {
+        allowNull: false,
+        type: DataTypes.NUMERIC,
+      },
+      cardNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    userId: {
-      type: DataTypes.INTEGER,
-    },
-    transactionTtpe: {
-      type: DataTypes.TEXT
-    },
-    amountOfMoneyTransferred: {
-      type: DataTypes.DECIMAL
-    },
-    createdAt: {
-      allowNull: false,
-      type: DataTypes.DATE
-    },
-    updatedAt: {
-      allowNull: false,
-      type: DataTypes.DATE
+    {
+      sequelize,
+      modelName: 'Transaction',
     }
-  }, {
-    sequelize,
-    modelName: 'TransactionHistory',
-  });
-  return TransactionHistory;
+  );
+  return Transaction;
 };
