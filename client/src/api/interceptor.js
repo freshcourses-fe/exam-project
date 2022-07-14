2e8d389a55b9cf9cd66187a7a8724e53dd92145e
Kirill <freshjs.ftd.@gmail.com>
2022-07-14 18:32:14 +0300

    Refactor interceptors

--- client/src/api/interceptor.js
+++ client/src/api/interceptor.js
@@ -2,28 +2,53 @@ import axios from 'axios';
 import CONTANTS from '../constants';
 import history from '../browserHistory';
 
-const instance = axios.create({
+const httpClient = axios.create({
   baseURL: CONTANTS.BASE_URL,
 });
 
-instance.interceptors.request.use((config) => {
-  const token = window.localStorage.getItem(CONTANTS.ACCESS_TOKEN);
-  if (token) {
-    config.headers = { ...config.headers, Authorization: token };
-  }
-  return config;
-}, (err) => Promise.reject(err));
+httpClient.interceptors.request.use(
+  config => {
+    const token = window.localStorage.getItem(CONTANTS.ACCESS_TOKEN);
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  err => Promise.reject(err)
+);
 
-instance.interceptors.response.use((response) => {
-  if (response.data.token) {
-    window.localStorage.setItem(CONTANTS.ACCESS_TOKEN, response.data.token);
-  }
-  return response;
-}, (err) => {
-  if (err.response.status === 408 && history.location.pathname !== '/login' && history.location.pathname !== '/registration' && history.location.pathname !== '/') {
-    history.replace('/login');
-  }
-  return Promise.reject(err);
-});
+httpClient.interceptors.response.use(
+  response => {
+    if (response.data.tokenPair) {
+      const { accessToken, refreshToken } = response.data.tokenPair;
 
-export default instance;
+      window.localStorage.setItem(CONTANTS.ACCESS_TOKEN, accessToken);
+      window.localStorage.setItem(CONTANTS.REFRESH_TOKEN, refreshToken);
+    }
+    return response;
+  },
+  async err => {
+    const refresh = localStorage.getItem(CONTANTS.REFRESH_TOKEN);
+    if (err.response.status === 419 && refresh) {
+      const res = await httpClient.post('/auth/refresh', {
+        refreshToken: refresh,
+      });
+
+      const {
+        data: {
+          tokenPair: { accessToken, refreshToken },
+        },
+      } = res;
+
+      window.localStorage.setItem(CONTANTS.ACCESS_TOKEN, accessToken);
+      window.localStorage.setItem(CONTANTS.REFRESH_TOKEN, refreshToken);
+
+      err.config.headers['Authorization'] = `Bearer ${accessToken}`;
+
+      return axios.request(err.config);
+    }
+    return Promise.reject(err);
+  }
+);
+
+export default httpClient;
